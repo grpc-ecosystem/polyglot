c2398f68792794c496273d51e0d2e7aa91ec4f3f
Dino Wernli <dinowernli@gmail.com>
2016-03-12 12:30:51 +0000

    Replace raw stdout with proper logging.

--- WORKSPACE
+++ WORKSPACE
@@ -12,3 +12,13 @@ maven_jar(
   name = "junit_artifact",
   artifact = "junit:junit:4.10",
 )
+
+maven_jar(
+  name = "slf4j_api_artifact",
+  artifact= "org.slf4j:slf4j-api:1.7.13",
+)
+
+maven_jar(
+  name = "slf4j_simple_artifact",
+  artifact = "org.slf4j:slf4j-simple:1.7.13",
+)
--- src/main/java/polyglot/Main.java
+++ src/main/java/polyglot/Main.java
@@ -1,7 +1,12 @@
 package polyglot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello polyglot");
+    logger.info("Hello polyglot");
   }
 }
