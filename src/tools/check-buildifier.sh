d2ae8c99563fb1b0fd3273144786da4b4907eae6
Dino Wernli <dinowernli@gmail.com>
2019-07-30 15:25:05 +0700

    Port the codebase to the official proto rules, modern grpc, and modern bazel.

--- src/tools/check-buildifier.sh
+++ src/tools/check-buildifier.sh
@@ -8,9 +8,8 @@ else
   MODE="-mode=check"
 fi
 
-
 bazel build @com_github_bazelbuild_buildtools//buildifier:buildifier
-RESULT=`find . -name BUILD -or -name WORKSPACE | xargs bazel-bin/external/com_github_bazelbuild_buildtools/buildifier/buildifier $MODE -v`
+RESULT=`find . -name BUILD -or -name WORKSPACE | xargs bazel-bin/external/com_github_bazelbuild_buildtools/buildifier/darwin_amd64_stripped/buildifier $MODE -v`
 
 # In theory, buildifier should return a non-zero exit code in check mode if there are errors. From
 # staring at the buildifier code, it seems buildifier only returns the correct exit code for the
